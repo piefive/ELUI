711002145a4829a28977728007449b35210b5f91
Nabos <nabos.dezigner@gmail.com>
2022-03-21 18:37:58 +0300

    feat: add slider wrapper

--- src/components/RangeInput/RangeInput.tsx
+++ src/components/RangeInput/RangeInput.tsx
@@ -1,19 +1,32 @@
 import { forwardRef } from 'react';
 
-import { combineClassNames } from 'lib';
+import { combineClassNames, isNaN } from 'lib';
 import { FieldBox } from 'internal';
 
 import type { IRangeInput } from './types';
 import { RANGE_INPUT_CN } from './constants';
+import { RangeSlider } from './units';
+import { StyledInput, StyledRangeSliderBox, StyledSliderSlider } from './styled';
 
 export const RangeInput = forwardRef<HTMLInputElement, IRangeInput>(
-  ({ className, validate = null, validateMessage, message, label, isRequired, boxStyle }, rangeInputRef) => {
+  (
+    { className, validate = null, validateMessage, message, label, isRequired, boxStyle, min = 0, max = 100, ...rest },
+    rangeInputRef
+  ) => {
+    const numberValue = Number(rest.value);
+    const percent = isNaN(numberValue) ? 0 : ((numberValue - min) / (max - min)) * 100;
+
     return (
       <FieldBox
         className={combineClassNames(className, RANGE_INPUT_CN)}
         {...{ label, isRequired, validate, validateMessage, message, boxStyle }}
       >
-        stub
+        <StyledRangeSliderBox>
+          <StyledSliderSlider style={{ width: `${percent}%` }}>
+            <RangeSlider />
+          </StyledSliderSlider>
+        </StyledRangeSliderBox>
+        <StyledInput ref={rangeInputRef} {...rest} />
       </FieldBox>
     );
   }
---
+++ src/components/RangeInput/styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const StyledSliderSlider = styled.div`
+  position: relative;
+  height: 100%;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.palette.primary_700};
+`;
+
+export const StyledRangeSliderBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.palette.grey_200};
+`;
+
+export const StyledInput = styled.input`
+  position: absolute;
+  left: -99999px;
+  width: 0;
+  height: 0;
+  border: 0;
+  background-color: transparent;
+`;
