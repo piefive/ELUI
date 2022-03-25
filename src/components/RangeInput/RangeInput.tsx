9c2068facc02a8690de1edbab635168fe9782daf
Nabos <nabos.dezigner@gmail.com>
2022-03-25 16:43:15 +0300

    feat: fix range-input drag

--- src/components/RangeInput/RangeInput.tsx
+++ src/components/RangeInput/RangeInput.tsx
@@ -7,7 +7,7 @@ import { bindAria, combineClassNames, createEventFn, noop, useForkForwardedRef }
 import { FieldBox } from 'internal';
 
 import type { IRangeInput } from './types';
-import { RANGE_INPUT_CN } from './constants';
+import { RANGE_CHEVRON_ICON_SIZE, RANGE_INPUT_CN } from './constants';
 import { useRangeHandlers, useRatio } from './hooks';
 import { StyledInput, StyledRail, StyledThumb, StyledThumbBox, StyledTrack } from './styled';
 
@@ -39,7 +39,10 @@ export const RangeInput = forwardRef<HTMLInputElement, IRangeInput>(
     const handlers = useRangeHandlers({ innerRef, railRef, min, max, value: validValue });
     const ratio = useRatio(min, max, validValue);
 
-    const bindThumb = useDrag(({ xy: [x], down }) => down && !disabled && handlers.handleChangeTrack(x), { axis: 'x' });
+    const bindThumb = useDrag(
+      ({ xy: [x], down }) => down && !disabled && handlers.handleChangeTrack(x + RANGE_CHEVRON_ICON_SIZE),
+      { axis: 'x' }
+    );
 
     return (
       <FieldBox
