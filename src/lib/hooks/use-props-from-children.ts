4874fdf4777e4a6d22df9e537e6aea176621a24a
Nabos <nabos.dezigner@gmail.com>
2022-03-28 19:31:11 +0300

    feat: add hook (props from children)

---
+++ src/lib/hooks/use-props-from-children.ts
@@ -0,0 +1,20 @@
+import { Children, Component, FC, ReactElement, ReactNode, useMemo } from 'react';
+
+import { cast, isObject } from 'lib/utils';
+
+export const usePropsFromChildren = <Props extends Record<string, unknown>>(
+  children: ReactNode,
+  component: FC<Props>
+) =>
+  useMemo(
+    () =>
+      Children.toArray(children).reduce<Props[]>((acc, reactNode) => {
+        const isReactElement = isObject<ReactElement>(reactNode) && reactNode?.type === component;
+        console.log('render');
+
+        if (isReactElement) acc.push(cast<Component<Props>>(reactNode).props);
+
+        return acc;
+      }, []),
+    [children, component]
+  );
